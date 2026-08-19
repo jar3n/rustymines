c45033e98a323f5cf2cb7b3e080c8c322a030abe
James Englander <jamreng@proton.me>
2026-08-19 17:49:05 -0400

    Started building the start menu ui

--- src/ui.rs
+++ src/ui.rs
@@ -1,5 +1,20 @@
 
-use crate::app::App;
+use std::rc::Rc;
+
+use crate::app::{
+    App,
+    DifficultyLevel
+};
+
+use ratatui::{
+    layout::{
+        self, Constraint, Direction::Vertical, Layout, Rect
+    }, style::{Color, Style}, symbols, widgets::{
+        Block, BorderType, Borders, Paragraph
+    }
+};
+
+
 
 
 pub enum Views {
@@ -9,23 +24,67 @@ pub enum Views {
 
 // app render ui entry point
 pub fn render(app:&mut App, frame: &mut ratatui::Frame) {
-        
-        let menu_widget = match app.state() {
-            Views::Start => render_start(),
-            Views::InGame => render_game(),
+        match app.state() {
+            Views::Start => render_start(frame),
+            Views::InGame => render_game(frame),
         };
+}
 
-        frame.render_widget(menu_widget, frame.area());
+fn base_layout(frame: &mut ratatui::Frame) -> Rc<[Rect]>{
+    layout::Layout::default()
+            .constraints(vec![
+                Constraint::Percentage(66),
+                Constraint::Percentage(33)
+            ])
+            .split(frame.area())
+}
+
+pub fn widget_borders() -> Block<'static> {
+    Block::new()
+        .borders(Borders::ALL)
+        .border_style(
+            Style::new()
+            .green()
+            
+        )
+        .border_type(
+            BorderType::Rounded
+        )
 }
 
 // render start menu
-pub fn render_start() -> &'static str {
-    "Start Menu!!!!"
+pub fn render_start(frame: &mut ratatui::Frame) {
+    let base = base_layout(frame);
+
+    let title_layout = Layout::default()
+                                    .direction(Vertical)
+                                    .constraints(vec![
+                                        Constraint::Fill(1),
+                                        Constraint::Fill(1),
+                                        Constraint::Fill(1),
+                                    ])
+                                    .split(base[0]);
+
+    let top_text = Paragraph::new("Rusty Mines")
+                                        .centered()
+                                        .block(widget_borders());
+
+    frame.render_widget(top_text, title_layout[1]);
+
+    let bottom_text = Paragraph::new("start menu options go here")
+                                        .centered()
+                                        .block(widget_borders());
+
+    frame.render_widget(bottom_text, base[1]);
 }
 
 // render in game menu
-pub fn render_game() -> &'static str{
-    "Game"
+pub fn render_game(frame: &mut ratatui::Frame) {
+    let base = base_layout(frame);
+
+    let text = Paragraph::new("game with paragraph");
+
+    frame.render_widget(text, base[0]);
 }
 
 // might have fail and succeed 
